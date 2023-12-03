338fcff5ff85d12fb6eca4f476d5d7be8770330e
Viktor Popov <popow_viktor@abv.bg>
2023-12-03 15:56:08 +0200

    renamed the main buttons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,10 @@
 <h1>Добре дошли !</h1>
 <p>Фирма за климатици, която ще Ви предостави добро обслужване, <br> качествени продукти на добри цени. </p>
 <div>
-    <a href="./promotions">
+    <a href="./промоции">
         <button type="button">ПРОМОЦИИ</button>
     </a>
-    <a href="/catalog">
+    <a href="/каталог">
         <button type="button">КАТАЛОГ</button>
     </a>
 </div>
