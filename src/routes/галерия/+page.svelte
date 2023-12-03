a59421cbd5fa2f3073f9a1452b73e2b4261ccc17
Viktor Popov <popow_viktor@abv.bg>
2023-12-03 23:25:12 +0200

    renamed images

--- src/routes/галерия/+page.svelte
+++ src/routes/галерия/+page.svelte
@@ -1,6 +1,7 @@
 <!--TASKOVE
 -Zoom unzoom button
--lightbox almost on full screen-->
+-lightbox almost on full screen
+-da sa na redove po 4-->
 
 
 <script>
@@ -8,7 +9,11 @@
       "/gallery/gallery1.jpg",
       "/gallery/gallery2.jpg",
       "/gallery/gallery3.jpg",
-      "/gallery/gallery4.jpg",
+      "/gallery/gallery4.jpg", 
+      "/gallery/gallery5.jpg",
+      "/gallery/gallery6.jpg",
+      "/gallery/gallery7.jpg",
+      "/gallery/gallery8.jpg",
     ];
   
     let selectedImage = null;
@@ -31,10 +36,10 @@
   
     .thumbnail {
       width: 150px;
-      height: 150px;
-      margin: 10px;
-      overflow: hidden;
-      cursor: pointer;
+    height: 150px; 
+    margin: 10px;
+    overflow: hidden;
+    cursor: pointer;
     }
   
     .thumbnail img {
